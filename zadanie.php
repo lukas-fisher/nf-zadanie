332ae43dd73104819cbc2d28ff3957a3f85b840b
lukas-fisher <lukas.ryy34.fisher@gmail.com>
2020-12-27 21:03:22 +0100

    startuję z plikiem

---
+++ zadanie.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["NFZ"]))
+ {
+   $_SESSION["NFZ"] = "15";
+ }
+ // ustawiam ręcznie oddział nfz dla późniejszej klasy abstrakcyjnej, aby móc skalować.
+
+ abstract class Nfz {
+
+ }
+
+
+?>
