18ef3f346952cfb891fd99053292c5cd0268fad9
lukas-fisher <lukas.ryy34.fisher@gmail.com>
2021-01-04 13:31:55 +0100

    rozwinięcie dalszych zmian

--- zadanie.php
+++ zadanie.php
@@ -15,14 +15,24 @@ if (!isset($_SESSION["NFZ"]))
 class Przychodnia extends Nfz {
   public $nazwa;
   public $miasto;
-  public $lista_pacjentow;
+  public $ulica;
+  public $numer_kontaktowy;
+  public $lista_pacjentow = [];
+
+  public function __construct($nazwa, $miasto, $ulica, $numer_kontaktowy, $lista_pacjentow) {
+    $this->nazwa = $nazwa;
+    $this->miasto = $miasto;
+    $this->ulica = $ulica;
+    $this->numer_kontaktowy = $numer_kontaktowy;
+    $this->lista_pacjentow = $lista_pacjentow;
+  }
 }
 
 class Pacjent extends Przychodnia {
   public $nazwisko;
   public $imie;
   public $pesel;
-  public $choroby;
+  public $choroby = [];
 }
 
 
@@ -51,19 +61,70 @@ class Pacjent extends Przychodnia {
 </div>
 <div>
 <?php
+// kolumna 2
+
  if (isset($_GET['przychodnia']))
   {
-    print "przychodnia menu";
+    print "<p>przychodnia menu</p>";
+
+    if ($_GET['przychodnia'] == "lista")
+     {
+       print "<p>lista przychodni</p>";
+     }
+    else if ($_GET['przychodnia'] == "dodaj")
+     {
+       print "<p>dodaj przychodnię</p>";
+     }
+    else if ($_GET['przychodnia'] == "przypisz")
+     {
+       print "<p>ocdczytuje pacjentów nieprzypisanych</p>";
+     }
   }
 else if (isset($_GET['pacjent']))
  {
-   print "pacjent menu";
+   print "<p>pacjent menu</p>";
+
+   if ($_GET['pacjent'] == "lista")
+    {
+      print "<p>lista pacjentów</p>";
+    }
+   else if ($_GET['pacjent'] == "dodaj")
+    {
+      print "<p>dodaj pacjenta</p>";
+    }
  }
 ?>
 </div>
 <div>
 <?php
+//kolumna 3
 
+if (isset($_GET['przychodnia']))
+ {
+   if ($_GET['przychodnia'] == "lista")
+    {
+      print "<p>mechniaka list</p>";
+    }
+   else if ($_GET['przychodnia'] == "dodaj")
+    {
+      print "<p>dodawanie przychodni</p>";
+    }
+   else if ($_GET['przychodnia'] == "przypisz")
+    {
+      print "<p>lista ludzików</p>";
+    }
+ }
+else if (isset($_GET['pacjent']))
+{
+  if ($_GET['pacjent'] == "lista")
+   {
+     print "<p>lista wszytskich pacentów</p>";
+   }
+  else if ($_GET['pacjent'] == "dodaj")
+   {
+     print "<p>formularz dodawania pacjenta</p>";
+   }
+}
 ?>
 </div>
 
